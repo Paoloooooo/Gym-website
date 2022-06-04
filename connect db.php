2020c317ce7122250dc5dc00918b009fe7763fa4
Paolo <68144630+Paoloooooo@users.noreply.github.com>
2022-06-04 18:26:13 +0200

    Delete connect db.php

--- connect db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$db_db = 'tsw';
-$username_db = 'www';
-$password_db = 'tsw2022';
-$connection_string = "host=$host dbname=$db_db user=$username_db
-password=$password_db";
-?> 
